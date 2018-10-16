7e0a956adc753d33bdbacd015d44e09d8f18ca1e
sklin93 <sklin@connect.ust.hk>
2018-10-16 02:39:32 -0700

    add infer

--- cc_fista.py
+++ cc_fista.py
@@ -1,5 +1,6 @@
 import numpy as np
-import scipy.linalg as LA
+from scipy.linalg import norm
+from math import sqrt
 
 def standardize(D):
 	S = D-np.tile(D.mean(axis=0),(D.shape[0],1))
@@ -14,6 +15,10 @@ def sthreshmat(x,tau,t):
 	assert x.shape == t.shape, 'matrix shape mismatch'
 	return np.sign(x) * np.maximum(abs(x)-tau*t, 0.0)
 
+def pseudol(X,W):
+	return -np.log(X.diagonal()).sum() + 0.5*(X@W).trace()
+
+#TODO: verbosity
 class cc_fista(object):
 	"""concord fista"""
 	def __init__(self, D, lam, pMat=None, 
@@ -27,11 +32,11 @@ class cc_fista(object):
 		else:
 			self.S = standardize(D)
 		# penalty
-		self.lamdaMat = lam*np.ones((p,p))
+		self.LambdaMat = lam*np.ones((p,p))
 		if not penalize_diag:
-			np.fill_diagonal(self.lamdaMat, 0)
+			np.fill_diagonal(self.LambdaMat, 0)
 		if pMat is not None:
-			self.lamdaMat[pMat==0] *= 100
+			self.LambdaMat[pMat==0] *= 100
 		# other init
 		self.X0 = np.identity(p)
 		self.tol = tol
@@ -44,22 +49,66 @@ class cc_fista(object):
 		Theta = self.X0.copy()
 		W = self.S@X
 		WTh = self.S@Theta
-		h = -np.log(X.diagonal()).sum() + 0.5*(X@W).trace()
+		h = pseudol(X,W)
 		# const init
 		hn = hTh = Qn = f = 0.0
 		taun = alpha = 1.0
 		c = 0.9
 		itr = diagitr = backitr = 0
-		loop = 1
+		loop = True
 
 		G = 0.5 * (WTh + WTh.transpose())
-  		G += - np.diag(1.0/Theta.diagonal())
+		G += - np.diag(1.0/Theta.diagonal())
 
-  		while loop != 0:
-  			tau = taun
-  			diagitr = backitr = 0
+		while loop:
+			tau = taun
+			diagitr = backitr = 0
 
-  			while True:
-  				if diagitr != 0 or backitr != 0: 
-  					tau = tau * c
-  				tmp = Theta - tau*G
+			while True:
+				if diagitr != 0 or backitr != 0: 
+					tau = tau * c
+
+				Xn = sthreshmat(Theta-tau*G, tau, self.LambdaMat);
+				if Xn.diagonal().min()<1e-8 and diagitr<50: # Q mark, thresh by LambdaMat, always have 0? always enter if?
+					diagitr += 1
+					continue
+
+				Step = Xn - Theta
+				hTh = pseudol(Theta,WTh)
+				Qn = hTh + (Step*G).sum() + (1/(2*tau))*(norm(Step)**2)
+				Wn = self.S @ Xn
+				hn = pseudol(Xn,Wn)
+				if hn > Qn:
+					backitr += 1
+				else:
+					break
+
+			alphan = (1 + sqrt(1+4*(alpha**2)))/2;
+			Theta = Xn + ((alpha-1)/alphan) * (Xn-X)
+			WTh = S@Theta
+			Gn = 0.5 * (WTh + WTh.transpose())
+			Gn += - np.diag(1.0/Theta.diagonal())
+
+			if self.steptype == 0:
+				taun = 1
+			elif self.steptype == 1:
+				taun = tau
+			elif self.steptype == 2:
+				taun = (Step@Step).diagonal().sum() / (Step@(Gn-G)).trace()
+				if taun < 0.0:
+					taun = tau
+			# compute subg
+			tmp = Gn + np.sign(Xn)*self.LambdaMat
+			subg = sthreshmat(Gn, 1.0, self.LambdaMat)
+			subg[Xn!=0] = tmp[Xn!=0]
+			subgnorm = norm(subg)
+			Xnnorm = norm(Xn)
+			# iteration update
+			alpha = alphan
+			X = Xn
+			h = hn
+			G = Gn
+			f = h + (abs(Xn)*self.LambdaMat).sum()
+			itr += 1
+			loop = itr<self.maxit and subgnorm/Xnnorm>tol
+		return Xn
