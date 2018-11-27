054622ee1ff2e816179a3a4486c253985c1b5f5b
sklin93 <happlesky@gmail.com>
2018-11-26 20:19:46 -0800

    f reconstruct clean up

--- f_recon.py
+++ f_recon.py
@@ -18,22 +18,10 @@ k = np.count_nonzero(omega) # number of model parameters.
 
 X = Variable((p,p))
 constraints = [X[omega==0]==0]
-# X = omega.copy()
-# import ipdb; ipdb.set_trace()
 
-# loss = 0
 pred_f = X.__matmul__(vec_s.T)
 loss = norm(vec_f-pred_f.T)
-# for subj_idx in range(n):
-# 	print(subj_idx)
-# 	pred_f = X.__matmul__(vec_s[subj_idx].reshape(-1,1))
-# 	loss += norm(vec_f[subj_idx] - pred_f)**2
-# 	# cur_w_pos = 0
-# 	# for f_idx in range(p):
-# 	# 	cur_w_num = np.count_nonzero(omega[f_idx])
-# 	# 	pred_f = sum(multiply(vec_s[subj_idx][omega[f_idx].astype(bool)],w[cur_w_pos:cur_w_pos+cur_w_num]))
-# 	# 	cur_w_pos += cur_w_num
-# 	# 	loss += norm(vec_f[subj_idx][f_idx] - pred_f)**2
+
 lambd = 0.01
 reg = norm(X, 2)
 prob = Problem(Minimize(loss/n + lambd*reg),constraints)
