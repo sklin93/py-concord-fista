1febcb6184d952db24ee10ac316cbde1994f7de5
hyou <hyou@cs.ucsb.edu>
2019-03-12 14:12:44 -0700

    sync

--- preproc/replenish.sh
+++ preproc/replenish.sh
@@ -7,7 +7,7 @@ do
     for phase in "${PHASE_ENCODING[@]}"
     do
         output_dir="/work/code/fs125/$subject/timeseries/tfMRI_LANGUAGE_125mm_$phase/Lausanne2008/ROI_scale33"
-        3dmaskdump -xyz -mask $output_dir/8.nii.gz /work/code/fs125/$subject/tfMRI/tfMRI_LANGUAGE_125mm_$phase.nii.gz > $output_dir/8.txt
+        3dmaskdump -xyz -mask work/code/fs125/atlas_mask/Lausanne2008/ROI_scale33/8.nii.gz /work/code/fs125/$subject/tfMRI/tfMRI_LANGUAGE_125mm_$phase.nii.gz > $output_dir/8.txt
         [ -f $output_dir/timeseries_mean.ts ] && rm $output_dir/timeseries_mean.ts
         [ -f $output_dir/corrmat.fc ] && rm $output_dir/corrmat.fc
         python ./average_timeseries.py $output_dir timeseries_mean.ts
