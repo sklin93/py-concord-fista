0a70a7d7e9c744a88b68622113173ad53fe0dc1c
Hongyuan <hyou@daguan.local>
2019-06-03 01:58:36 -0700

    sync

---
+++ utility/network_inspect.py
@@ -0,0 +1,33 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+def plot_degree_distribution(G):
+    degs = {}
+    for n in G.nodes():
+        deg = G.degree(n)
+        pprint(deg)
+        if deg not in degs:
+            degs[deg] = 0
+        degs[deg] += 1
+    items = sorted(degs.items())
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter([k for (k,v) in items], [v for (k,v) in  items ])
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    plt.title("Degree Distribution")
+    plt.show()
+    fig.savefig("degree_distribution.png")
+
+def ba_test():
+    ba = nx.random_graphs.barabasi_albert_graph(3404,30)
+    plot_degree_distribution(ba)
+    return ba
+
+# pos = nx.spring_layout(ba)
+# nx.draw(ba, pos, node_size = 30)
+# nx.draw(ba, pos)
+# plt.show()
+
