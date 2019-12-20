c7f8e9eccea96186984c3fa5bc89eb472e081c41
sklin93 <happlesky@gmail.com>
2019-12-20 14:38:36 -0800

    hcp mrce rnd B comparison

--- hcp_mrce.py
+++ hcp_mrce.py
@@ -30,68 +30,10 @@ with open('config.yaml') as info:
 
 fdir = 'fs_results/'
 
-def reconstruct_err(task, filename, rnd_compare=False):
-	vec_s, vec_f = data_prep(task)
-	n = vec_s.shape[0]
-	omega = np.load(filename)
-	d = int(omega.shape[1]/2)
-
-	# vec = np.concatenate((vec_f,vec_s), axis=1)
-	# S = standardize(vec)
-	# print((omega.transpose()*(S@omega.transpose())).sum())
-
-	omega = omega[:d,d:]
-	print(omega.min(),omega.mean(),omega.max())
-	print(omega.shape)
-	# rho = np.zeros(omega.shape)
-	# for i in range(d):
-	# 	for j in range(d):
-	# 		if i==j:
-	# 			rho[i,j] = 1
-	# 		else:
-	# 			rho[i,j] = -omega[i,j]/np.sqrt(omega[i,i]*omega[j,j])
-	print(np.count_nonzero(omega))
-	pred_f = vec_s@omega.T
-	print(pred_f.shape)
-	for k in range(n):
-		print(pearsonr(pred_f[k],vec_f[k]))
-
-	if rnd_compare:
-		# create random 'omega' with a same level of sparsity
-		rnd_w = np.zeros(omega.shape)
-		idx = np.random.choice(d*d,np.count_nonzero(omega),replace=False)
-		ctr = 0
-		for i in range(d):
-			for j in range(d):
-				if ctr in idx:
-					rnd_w[i,j] = 1
-				ctr += 1
-		print(np.count_nonzero(rnd_w))
-		pred_f_rnd = vec_s@rnd_w.T
-		print(pred_f_rnd.shape)
-		for k in range(n):
-			print(pearsonr(pred_f_rnd[k],vec_f[k]))
-		
-	# err = tot_err_perct = 0.0
-	# for k in range(n):
-	# 	cur_s = vec_s[k]
-	# 	f_gt = vec_f[k]
-	# 	f_model = np.zeros(d)
-	# 	for i in range(d):
-	# 		f_model[i] = - (omega[i].dot(cur_s))/omega[i,i]
-	# 	print(f_model.min(),f_model.mean(),f_model.max())
-	# 	# cur_err = norm(f_model-f_gt)
-	# 	# err_perct = cur_err/norm(f_gt)
-	# 	# print(err_perct)
-	# 	print(pearsonr(f_model,f_gt))
-		# tot_err_perct += err_perct
-		# err += cur_err
-	# print('average error percentage',tot_err_perct/n)
-	# return err
-
 def mrce_sf(task, b_lam=0.1, omega_lam=0.1, cross_val=10, load=False, 
 			OUT_MAX_ITR=20, out_verbose=False, plot_verbose=False,
-			eval_verbose=True):
+			eval_verbose=True, train_val=True, rnd_compare=False,
+			check_common=False):
 
 	def get_B_omega(vec_s, vec_f, pMat=None, b_lam=b_lam, omega_lam=omega_lam):
 		p = vec_s.shape[1]
@@ -143,20 +85,25 @@ def mrce_sf(task, b_lam=0.1, omega_lam=0.1, cross_val=10, load=False,
 
 	def recon_eval(vec_s, vec_f, B, Omg, b_lam):
 		pred_f = np.matmul(vec_s, B)
-		# recon error
-		err = LA.norm(vec_f - pred_f)
-		# correlation coefficient
+
+		# recon error percentage wrt ground truth
+		# err_percent = (LA.norm(vec_f - pred_f) ** 2) / (LA.norm(vec_f) ** 2)
+		# err_percent = (vec_f - pred_f).sum() / vec_f.sum()
+		err_percent = []
+
+		# correlation coefficient and p-value
 		avg_r = []
-		min_p = 1
-		max_p = 0
+		min_pval = 1
+		max_pval = 0
 		for i in range(len(vec_s)):
-			r, p = pearsonr(pred_f[i],vec_f[i])
-			# print(r, p)
+			err_percent.append((vec_f[i] - pred_f[i]).sum() / vec_f[i].sum())
+			r, pval = pearsonr(pred_f[i],vec_f[i])
 			avg_r.append(r)
-			if p < min_p:
-				min_p = p
-			if p > max_p:
-				max_p = p
+			if pval < min_pval:
+				min_pval = pval
+			if pval > max_pval:
+				max_pval = pval
+		err_percent = sum(err_percent)/len(err_percent)
 		avg_r = sum(avg_r)/len(avg_r)
 		# likelihood
 		problem = mrce(Omg=np.linalg.matrix_power(Omg,2),
@@ -165,7 +112,7 @@ def mrce_sf(task, b_lam=0.1, omega_lam=0.1, cross_val=10, load=False,
             c=0.5, alpha=1, TOL_ep=0.05, max_itr=50,
             verbose=False, verbose_plots=False)
 		obj = problem.likelihood_B(B)
-		return err, avg_r, min_p, max_p, obj
+		return err_percent, avg_r, min_pval, max_pval, obj
 
 
 	vec_s, vec_f = data_prep(task, v1=True, normalize_s=True)
@@ -193,18 +140,29 @@ def mrce_sf(task, b_lam=0.1, omega_lam=0.1, cross_val=10, load=False,
 		# k-fold cross_val
 		val_num = len(vec_s) // cross_val
 
-		train_err_avg	=	[]
-		train_r_avg 	=	[]
-		train_obj_avg 	=	[]
-		train_p_min 	=	1
-		train_p_max 	=	0
+		if train_val:
+			train_err_avg	=	[]
+			train_r_avg 	=	[]
+			train_obj_avg 	=	[]
+			train_p_min 	=	1
+			train_p_max 	=	0
 
-		val_err_avg	=	[]
-		val_r_avg 	=	[]
-		val_obj_avg =	[]
-		val_p_min 	=	1
-		val_p_max 	=	0
+			val_err_avg	=	[]
+			val_r_avg 	=	[]
+			val_obj_avg =	[]
+			val_p_min 	=	1
+			val_p_max 	=	0
 
+		if rnd_compare:
+			rnd_err_avg	=	[]
+			rnd_r_avg 	=	[]
+			rnd_obj_avg =	[]
+			rnd_p_min 	=	1
+			rnd_p_max 	=	0
+
+		if check_common:
+			common_omg = []
+			common_B = []
 		for i in range(cross_val):
 			print('cross validation, iteration:', i)
 			val_s = vec_s[i * val_num: (i+1) * val_num]
@@ -222,6 +180,15 @@ def mrce_sf(task, b_lam=0.1, omega_lam=0.1, cross_val=10, load=False,
 				np.save(b_name, B)
 				np.save(omg_name, Omg)
 			
+			if check_common:
+				omg_nz = Omg.copy()
+				omg_nz[omg_nz!=0] = 1
+				common_omg.append(omg_nz)
+
+				B_nz = B.copy()
+				B_nz[B_nz!=0] = 1
+				common_B.append(B_nz)
+
 			if eval_verbose:
 				# Stats of estimated B and Omega
 				print('B min:', B.min(), 'B max:', B.max(), 
@@ -230,50 +197,126 @@ def mrce_sf(task, b_lam=0.1, omega_lam=0.1, cross_val=10, load=False,
 						'Omg nz entry #:', np.count_nonzero(Omg))
 
 			# evaluate
-			# training data stats (as baseline)
-			err, avg_r, min_p, max_p, obj = recon_eval(train_s, train_f, B, Omg, b_lam)
-			if eval_verbose:
-				print('train recon err:', err, '; correlation coefficient:', avg_r, 
-					'; min/max p value:', min_p, max_p, '; obj:', obj)
-			train_err_avg.append(err)
-			train_r_avg.append(avg_r)
-			train_obj_avg.append(obj)
-			if min_p < train_p_min:
-				train_p_min = min_p
-			if max_p > train_p_max:
-				train_p_max = max_p
-			# validation data stats
-			err, avg_r, min_p, max_p, obj = recon_eval(val_s, val_f, B, Omg, b_lam)
-			if eval_verbose:
-				print('val recon err:', err, '; correlation coefficient:', avg_r, 
-					'; min/max p value:', min_p, max_p, '; obj:', obj)
-			val_err_avg.append(err)
-			val_r_avg.append(avg_r)
-			val_obj_avg.append(obj)
-			if min_p < val_p_min:
-				val_p_min = min_p
-			if max_p > val_p_max:
-				val_p_max = max_p
+			if train_val:
+				# training data stats (as baseline)
+				err, avg_r, min_p, max_p, obj = recon_eval(train_s, train_f, B, Omg, b_lam)
+				if eval_verbose:
+					print('train recon err:', err, '; correlation coefficient:', avg_r, 
+						'; min/max p value:', min_p, max_p, '; obj:', obj)
+				train_err_avg.append(err)
+				train_r_avg.append(avg_r)
+				train_obj_avg.append(obj)
+				if min_p < train_p_min:
+					train_p_min = min_p
+				if max_p > train_p_max:
+					train_p_max = max_p
+				# validation data stats
+				err, avg_r, min_p, max_p, obj = recon_eval(val_s, val_f, B, Omg, b_lam)
+				if eval_verbose:
+					print('val recon err:', err, '; correlation coefficient:', avg_r, 
+						'; min/max p value:', min_p, max_p, '; obj:', obj)
+				val_err_avg.append(err)
+				val_r_avg.append(avg_r)
+				val_obj_avg.append(obj)
+				if min_p < val_p_min:
+					val_p_min = min_p
+				if max_p > val_p_max:
+					val_p_max = max_p
 
-		train_err_avg	=	sum(train_err_avg)/len(train_err_avg)
-		train_r_avg 	=	sum(train_r_avg)/len(train_r_avg)
-		train_obj_avg	=	sum(train_obj_avg)/len(train_obj_avg)
-		print('***TRAIN DATA***', cross_val, 'fold recon error average:', 
-			train_err_avg, '; r average:', train_r_avg, 
-			'; min/max p value:', train_p_min, train_p_max,
-			'; average obj:', train_obj_avg)
+			if rnd_compare:
+				# random comparison (random B & Omg w/ same sparsity level)
+				p, q = B.shape
+				rnd_B = np.zeros((p, q))
+				idx = np.random.choice(p * q, np.count_nonzero(B), replace=False)
+				ctr = 0
+				for i in range(p):
+					for j in range(q):
+						if ctr in idx:
+							rnd_B[i,j] = 1
+						ctr += 1
+				err, avg_r, min_p, max_p, obj = recon_eval(val_s, val_f, rnd_B, Omg, b_lam)
+				if eval_verbose:
+					print('val recon err with random B and Omega:', err, 
+						'; correlation coefficient:', avg_r, 
+						'; min/max p value:', min_p, max_p, '; obj:', obj)
+				rnd_err_avg.append(err)
+				rnd_r_avg.append(avg_r)
+				rnd_obj_avg.append(obj)
+				if min_p < rnd_p_min:
+					rnd_p_min = min_p
+				if max_p > rnd_p_max:
+					rnd_p_max = max_p
 
-		val_err_avg	=	sum(val_err_avg)/len(val_err_avg)
-		val_r_avg 	=	sum(val_r_avg)/len(val_r_avg)
-		val_obj_avg	=	sum(val_obj_avg)/len(val_obj_avg)
-		print('***VAL DATA***', cross_val, 'fold recon error average:', 
-			val_err_avg, '; r average:', val_r_avg, 
-			'; min/max p value:', val_p_min, val_p_max,
-			'; average obj:', val_obj_avg)
+		# check common B and Omg
+		if check_common:
+			common_B	=	np.sum(np.stack(common_B), axis=0)
+			common_omg	=	np.sum(np.stack(common_omg), axis=0)
+
+			p, q = common_B.shape
+			B_nz_occr = {}
+			omg_nz_occr = {}
+
+			for i in range(p):
+				for  j in range(q):
+					if common_B[i, j] in B_nz_occr:
+						B_nz_occr[common_B[i, j]] += 1
+					else:
+						B_nz_occr[common_B[i, j]] = 1
+			for i in range(q):
+				for  j in range(q):
+					if common_omg[i, j] in omg_nz_occr:
+						omg_nz_occr[common_omg[i, j]] += 1
+					else:
+						omg_nz_occr[common_omg[i, j]] = 1
+			print('B nonzero occurrence across folds:')
+			for i in sorted(B_nz_occr):
+				print(i, ':', B_nz_occr[i])
+			print('Omega nonzero occurrence across folds:')
+			for i in sorted(omg_nz_occr):
+				print(i, ':', omg_nz_occr[i])
+
+			# save top common ones for visualization
+			print('top common ratio in B:', np.sum(common_B >= cross_val - 2) / np.count_nonzero(common_B))
+			common_B[common_B < cross_val - 2] = 0
+			common_B[common_B != 0] = 1
+			print('top common ratio in Omg:', np.sum(common_omg >= cross_val - 2) / np.count_nonzero(common_omg))
+			common_omg[common_omg < cross_val - 2] = 0
+			common_omg[common_omg != 0] = 1
+
+			np.save(fdir+str(b_lam)+'_mrce_B_'+task+'_common_vis'+'.npy', common_B)
+			np.save(fdir+str(omega_lam)+'_mrce_Omg_'+task+'_common_vis'+'.npy', common_omg)
+			# import ipdb; ipdb.set_trace()
+		# print average evaluation results for k-folds
+		if train_val:
+			train_err_avg	=	sum(train_err_avg)/len(train_err_avg)
+			train_r_avg 	=	sum(train_r_avg)/len(train_r_avg)
+			train_obj_avg	=	sum(train_obj_avg)/len(train_obj_avg)
+			print('***TRAIN DATA***', cross_val, 'fold recon error average:', 
+				train_err_avg, '; r average:', train_r_avg, 
+				'; min/max p value:', train_p_min, train_p_max,
+				'; average obj:', train_obj_avg)
+
+			val_err_avg	=	sum(val_err_avg)/len(val_err_avg)
+			val_r_avg 	=	sum(val_r_avg)/len(val_r_avg)
+			val_obj_avg	=	sum(val_obj_avg)/len(val_obj_avg)
+			print('***VAL DATA***', cross_val, 'fold recon error average:', 
+				val_err_avg, '; r average:', val_r_avg, 
+				'; min/max p value:', val_p_min, val_p_max,
+				'; average obj:', val_obj_avg)
+
+		if rnd_compare:
+			rnd_err_avg	=	sum(rnd_err_avg)/len(rnd_err_avg)
+			rnd_r_avg 	=	sum(rnd_r_avg)/len(rnd_r_avg)
+			rnd_obj_avg	=	sum(rnd_obj_avg)/len(rnd_obj_avg)
+			print('***VAL DATA WITH RAND B***', cross_val, 
+				'fold recon error average:', rnd_err_avg, 
+				'; r average:', rnd_r_avg, '; min/max p value:', 
+				rnd_p_min, rnd_p_max, '; average obj:', rnd_obj_avg)
 
 if __name__ == '__main__':
 	task = sys.argv[1]
 	b_lam = float(sys.argv[2])
 	omega_lam = float(sys.argv[3])
 
-	mrce_sf(task, b_lam=b_lam, omega_lam=omega_lam, load=True, eval_verbose=False)
+	mrce_sf(task, b_lam=b_lam, omega_lam=omega_lam, load=True, eval_verbose=False,
+		train_val=False, rnd_compare=False, check_common=True)
