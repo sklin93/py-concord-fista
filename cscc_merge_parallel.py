729bb475efb1719cdb8d999e619d8868f5d572d1
Hongyuan <hyou@cs.ucsb.edu>
2020-02-12 01:33:07 -0800

    cggm syntest

---
+++ cscc_merge_parallel.py
@@ -0,0 +1,1 @@
+'data-utility/syndata_n50p20q20.pkl'
---
+++ run_cggm_syntest.py
@@ -0,0 +1,136 @@
+import numpy as np
+import cvxpy as cvx
+import scipy.linalg as LA
+import sys, os, pickle, argparse, math, time, pickle
+from scipy.stats.stats import pearsonr
+
+
+
+'''helper functions'''
+from scipy.linalg import cho_factor, cho_solve, LinAlgError
+def check_pd(A, lower=True):
+    """
+    Checks if A is PD.
+    If so returns True and Cholesky decomposition,
+    otherwise returns False and None
+    """
+    try:
+        return True, np.tril(cho_factor(A, lower=lower)[0])
+    except LinAlgError as err:
+        if 'not positive definite' in str(err):
+            return False, None
+
+def chol_inv(B, lower=True):
+    """
+    Returns the inverse of matrix A, where A = B*B.T,
+    ie B is the Cholesky decomposition of A.
+    Solves Ax = I
+    given B is the cholesky factorization of A.
+    """
+    return cho_solve((B, lower), np.eye(B.shape[0]))
+
+def inv(A):
+    """
+    Inversion of a SPD matrix using Cholesky decomposition.
+    """
+    return chol_inv(check_pd(A)[1])
+
+
+
+def load_model_param(filepath):
+
+    with open(filepath) as fp:
+        '''Get the first line, which contains result information'''
+        line = fp.readline()
+        info = [int(s) for s in line.strip().split(' ')]
+        A = np.zeros((info[0],info[1]))
+        nz_info = info[2]
+        print('non-zero entry number: ', nz_info)
+        print('shape: ', A.shape)
+        while line:
+            line = fp.readline()
+            if line:
+                line_info = [float(s) for s in line.strip().split(' ')]
+                A[int(line_info[0]-1),int(line_info[1]-1)] = line_info[2]
+    return A
+
+
+def eval_cggm(pred_f, vec_f):
+    # correlation coefficient and p-value
+    
+	mpe = []
+	mape = []
+	avg_r = []
+	min_pval = 1
+	max_pval = 0
+	for i in range(len(vec_f)):
+		mpe.append((vec_f[i] - pred_f[i]).sum() / vec_f[i].sum())
+		mape.append((LA.norm(vec_f[i] - pred_f[i]) ** 2) / (LA.norm(vec_f) ** 2))
+		# mape.append(np.abs((vec_f[i] - pred_f[i]).sum()) / vec_f[i].sum())
+		r, pval = pearsonr(pred_f[i],vec_f[i])
+		avg_r.append(r)
+		if pval < min_pval:
+			min_pval = pval
+		if pval > max_pval:
+			max_pval = pval
+	
+	mpe = sum(mpe)/len(mpe)
+	mape = sum(mape)/len(mape)
+	avg_r = sum(avg_r)/len(avg_r)
+	return mpe, mape, avg_r, min_pval, max_pval
+
+
+
+def run_cggm(args):
+
+    with open(args.synthetic_data, 'rb') as pfile:
+        data = pickle.load(pfile)
+
+    prefix = os.path.splitext(os.path.basename(args.synthetic_data))[0]
+    wkdir       = "mega-cggm/demo/"+prefix
+    file_input  = wkdir+"/X_train.txt"
+    file_output = wkdir+"/Y_train.txt"
+    file_lambda = wkdir+"/lambda.txt"
+    file_theta  = wkdir+"/theta.txt"
+    file_stats  = wkdir+"/stats.txt"
+
+    os.system("mkdir "+wkdir)
+
+    # generate text files for X and Y
+    np.savetxt(file_input,  data.X, delimiter=" ")
+    np.savetxt(file_output, data.Y, delimiter=" ")
+
+    # run the program
+    if args.run_cggm:
+        command_str = "mega-cggm/AltNewtonCD/cggmfast_run -y "+str(args.lambda1)+" -x "+str(args.lambda2)+" -v 1 "+str(data.n)+" "+str(data.p)+" "+str(data.n)+" "+str(data.q)+" "+file_output+" "+file_input+" "+file_lambda+" "+file_theta+" "+file_stats
+        print(command_str)
+        os.system(command_str)
+    
+    # get results
+    theta = load_model_param(file_theta)
+    lambd = load_model_param(file_lambda)
+
+    # Do prediction of Y using Theta, Lambda given X_test
+    Y_pred = -np.dot(np.dot(inv(lambd), theta.T), data.X_test.T).T
+    mpe, mape, avg_r, min_p, max_p = eval_cggm(Y_pred, data.Y_test)
+    print('***VAL DATA***\nMPE:{0:.3e}\nMAPE:{1:.3e}\nCorr:{2:.3e}\nMin_pval:{3:.3e}\nMax_pval:{4:.3e}'.format(mpe, mape, avg_r, min_p, max_p))
+
+    return 
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Arguments for CGGM.')
+
+    parser.add_argument('--synthetic_data', type=str, default='data-utility/syndata_n50p20q20.pkl',
+                        help='File path to the synthetic dataset')
+    parser.add_argument('--lambda1', type=float, default=1,
+                        help='lambda1: penalty parameter for l_1')
+    parser.add_argument('--lambda2', type=float, default=1,
+                        help='lambda2: penalty parameter for l_2')
+    parser.add_argument('--run_cggm', default=False, action='store_true',
+                        help='Flag to call CGGM, otherwise simply evaluate')
+
+    args = parser.parse_args()
+    run_cggm(args)
+
